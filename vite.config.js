f0ed2ff5034671733f4a538cc1e3f0ed9aa0ea65
Kraaven <dhruv.bhojaraj@gmail.com>
2024-07-22 01:25:21 +0530

    Changed module head to 1

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import restart from 'vite-plugin-restart'
 
 export default {
-    root: 'src/Module3/', // Sources files (typically where index.html is)
+    root: 'src/Module1/', // Sources files (typically where index.html is)
     publicDir: '../static/', // Path from "root" to static assets (files that are served as they are)
     server:
     {
